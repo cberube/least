e2c15d4346bddb8b1a81d5f147257ee20b4b4fb5
Charles Berube <charles.berube@cascadeenergy.com>
2015-03-03 13:32:31 -0800

    Documentation pass

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ var objectPath = require('object-path');
 
 var least = {};
 
+/**
+ * Returns a function which expects to receive a `value` and assigns that
+ * value to the target property path on the given target object.
+ *
+ * The function returns the target object.
+ *
+ * @param {object} target The object to assign to
+ * @param {string} targetProperty The object path of the property to assign to
+ * @returns {Function}
+ */
 least.assign = function(target, targetProperty) {
   return function(value) {
     objectPath.set(target, targetProperty, value);
@@ -12,6 +22,18 @@ least.assign = function(target, targetProperty) {
   };
 };
 
+/**
+ * Returns a function which expects to receive a `source` object and assigns
+ * the value located at the given sourceProperty object path on the source
+ * object to the targetProperty object path on the target object.
+ *
+ * The function returns the target object.
+ *
+ * @param {string} sourceProperty The object path of the value to extract
+ * @param {object} target The target object for assignment
+ * @param {string} targetProperty The object path of the property to assign
+ * @returns {Function}
+ */
 least.pluckAndAssign = function(sourceProperty, target, targetProperty) {
   return function(source) {
     objectPath.set(
@@ -24,6 +46,18 @@ least.pluckAndAssign = function(sourceProperty, target, targetProperty) {
   };
 };
 
+/**
+ * Returns a function which expects to receive an accumulator and a source
+ * object and pushes the value at the sourceProperty object path on the source
+ * object onto the accumulator.
+ *
+ * If the accumulator is null, an empty array will be created.
+ *
+ * The function returns the accumulator.
+ *
+ * @param sourceProperty
+ * @returns {Function}
+ */
 least.accumulate = function(sourceProperty) {
   return function(accumulator, source) {
     if (!accumulator) {

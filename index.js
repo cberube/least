7982aacab9c0f2071586c060866a27b20ce43112
Charles Berube <charles.berube@cascadeenergy.com>
2015-03-03 15:17:32 -0800

    Naming convention update

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ var least = {};
  * The function returns the target object.
  *
  * @param {object} target The object to assign to
- * @param {string} targetProperty The object path of the property to assign to
+ * @param {string} targetPath The object path of the property to assign to
  * @returns {Function}
  */
-least.assign = function(target, targetProperty) {
+least.assign = function(target, targetPath) {
   return function(value) {
-    objectPath.set(target, targetProperty, value);
+    objectPath.set(target, targetPath, value);
 
     return target;
   };
@@ -24,22 +24,22 @@ least.assign = function(target, targetProperty) {
 
 /**
  * Returns a function which expects to receive a `source` object and assigns
- * the value located at the given sourceProperty object path on the source
- * object to the targetProperty object path on the target object.
+ * the value located at the given sourcePath object path on the source
+ * object to the targetPath object path on the target object.
  *
  * The function returns the target object.
  *
- * @param {string} sourceProperty The object path of the value to extract
+ * @param {string} sourcePath The object path of the value to extract
  * @param {object} target The target object for assignment
- * @param {string} targetProperty The object path of the property to assign
+ * @param {string} targetPath The object path of the property to assign
  * @returns {Function}
  */
-least.pluckAndAssign = function(sourceProperty, target, targetProperty) {
+least.pluckAndAssign = function(sourcePath, target, targetPath) {
   return function(source) {
     objectPath.set(
       target,
-      targetProperty,
-      objectPath.get(source, sourceProperty)
+      targetPath,
+      objectPath.get(source, sourcePath)
     );
 
     return target;
@@ -48,23 +48,23 @@ least.pluckAndAssign = function(sourceProperty, target, targetProperty) {
 
 /**
  * Returns a function which expects to receive an accumulator and a source
- * object and pushes the value at the sourceProperty object path on the source
+ * object and pushes the value at the sourcePath object path on the source
  * object onto the accumulator.
  *
  * If the accumulator is null, an empty array will be created.
  *
  * The function returns the accumulator.
  *
- * @param sourceProperty
+ * @param sourcePath
  * @returns {Function}
  */
-least.accumulate = function(sourceProperty) {
+least.accumulate = function(sourcePath) {
   return function(accumulator, source) {
     if (!accumulator) {
       accumulator = [];
     }
 
-    accumulator.push(objectPath.get(source, sourceProperty));
+    accumulator.push(objectPath.get(source, sourcePath));
 
     return accumulator;
   };
